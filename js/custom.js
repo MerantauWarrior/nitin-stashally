6c7ca5e423bf54fe06db257c4e6afc1e1f5fea9c
Boris <boris1488ss@gmail.com>
2020-04-01 01:00:46 +0300

    Initial commit

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+//  HEADER
+  $('.header-groups__body').click(function () {
+    $(this).parent().toggleClass('header-groups_opened');
+  });
+//  Cards Layout
+  $('.grid').imagesLoaded()
+    .done( function( instance ) {
+      $('.gallery__items').masonry({
+        itemSelector: '.grid-item'
+      });
+    });
+//  Big Tabs
+  $('.big-tabs-btn').click(function (e) {
+    e.preventDefault();
+    var tabName = $(this).attr('href');
+    $('.big-tabs-btn').removeClass('big-tabs-btn_active');
+    $(this).addClass('big-tabs-btn_active');
+    $('.big-tabs-tab').removeClass('big-tabs-tab_active');
+    $('.big-tabs-tab'+tabName).addClass('big-tabs-tab_active');
+  });
+//  Install Banner
+  $('.install-app__close').click(function () {
+    $('.install-app').remove();
+  })
+});
