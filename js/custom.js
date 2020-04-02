fdc9dec414094d43cf3cccee07d77494fb587c95
Boris <boris1488ss@gmail.com>
2020-04-02 11:21:22 +0300

    nitin remake

--- js/custom.js
+++ js/custom.js
@@ -19,8 +19,38 @@ $( document ).ready(function() {
     $('.big-tabs-tab').removeClass('big-tabs-tab_active');
     $('.big-tabs-tab'+tabName).addClass('big-tabs-tab_active');
   });
+//  Categories
+  $('.categories__items').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+    prevArrow: $('.categories__arrow-prev'),
+    nextArrow: $('.categories__arrow-next')
+  });
+  $('.categories__arrow-prev').hide();
+  $('.categories__arrow').click(function () {
+    var currentSlide = $('.categories__items').slick('slickCurrentSlide');
+    console.log(currentSlide);
+    if(currentSlide > 0){
+      $('.categories__arrow-prev').show();
+    }else {
+      $('.categories__arrow-prev').hide();
+    }
+  });
 //  Install Banner
   $('.install-app__close').click(function () {
     $('.install-app').remove();
-  })
+  });
+//  Modals
+  $('.js-modal-close').click(function () {
+    $('.modal').hide();
+    $('body').removeClass('ovh');
+  });
+//  Header menu
+  $('.header-btn-menu').click(function (e) {
+    e.preventDefault();
+    $('.header-menu').toggleClass('header-menu_opened');
+  });
 });
